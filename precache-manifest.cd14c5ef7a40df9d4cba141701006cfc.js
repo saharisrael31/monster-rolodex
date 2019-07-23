64a9f1b5af7e35c86fdc5a7da629affec6010e8f
sahar israel <saharisrael31@gmail.com>
2019-07-23 11:25:33 +0300

    Updates

---
+++ precache-manifest.cd14c5ef7a40df9d4cba141701006cfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef9cec5841b6bc8cabf0ec098613f13",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6cd369c9c334a15ecde4",
+    "url": "/monsters-rolodex/static/css/main.67d5e2e6.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6cd369c9c334a15ecde4",
+    "url": "/monsters-rolodex/static/js/main.2cdbcc26.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
